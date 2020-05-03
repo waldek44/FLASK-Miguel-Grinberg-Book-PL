c267deff218f68cd83c2d01bf115bef7253ccf0d
Waldek <reclikmail@gmail.com>
2020-05-03 16:49:35 +0200

    3 | SZABLONY

--- hello.py
+++ hello.py
@@ -1,13 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
+
+
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 
 @app.route('/')
 def index():
-    return '<p> to jest hello </p>'
+    return render_template('index.html', current_time=datetime.utcnow())
 
 
 # wprowadzam do paska http://127.0.0.1:5000/user/superman
 @app.route('/user/<name>')
 def user(name):
-    return '<p> Witaj {} </p>'.format(name)
+    return render_template('user.html', name=name)
+
